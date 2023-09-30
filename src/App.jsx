dd4f339b7f4b7a4e965c56163e33d099cf34e168
alxeandre <alexandre.mr.billerau@gmail.com>
2023-09-30 18:19:32 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+import LeitnerBox from "./leitnerBox";
+import { BaseDirectory, readTextFile } from "@tauri-apps/api/fs";
+
+const App = async () => {
+  //load all box one computer
+
+  const fakeBox = [
+    { title: "LeetCode", review: 2, day: 2, learned: 0 },
+    { title: "Math 1001", review: 5, day: 4, learned: 0 },
+  ];
+
+  return (
+    <>
+      <div className="flex">
+        <div className="flex flex-col white-bg h-[100vh] w-[330px]">
+          <div className="flex flex-col p-4">
+            <h1 className="europa-bold tracking-wide black-text">
+              Leitner Box
+            </h1>
+            <h2 className="euro-style accent-text -mt-2">ChunkLab</h2>
+            <h6 className="euro-style black-text -mt-2 mb-[20px]">
+              Mastering your subject 🔥🔥🔥
+            </h6>
+          </div>
+          <nav className="flex flex-col max-h-[700px] overflow-y-auto">
+            {fakeBox.map((boxLink, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex flex-col w-100 navLink p-4 lt-border-black cursor-pointer"
+                >
+                  <span className="text-[25px] europa-bold">
+                    {boxLink.title}
+                  </span>
+                  <span className="text-[15px] euro-style -mt-2">
+                    {boxLink.review} to review
+                  </span>
+                  <div className="flex mt-2 -ml-1">
+                    <span>🔥 Day : {boxLink.day} </span>
+                    <span className="ml-[21px]">
+                      💯 Learned : {boxLink.learned}{" "}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+          </nav>
+          <div className="flex justify-center">
+            <button className="button-new-box primary-bg europa-bold mt-5 w-[200px] px-[41px] py-[16px] rounded-[5px] text-[25px]">
+              NEW BOX
+            </button>
+          </div>
+        </div>
+        <LeitnerBox></LeitnerBox>
+        {/* content here */}
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/leitnerBox.jsx
@@ -0,0 +1,74 @@
+const LeitnerBox = ({ box = {} }) => {
+  return (
+    <>
+      <section className="w-[100%]">
+        <div className="secondary-bg flex pt-[20px]">
+          <div className="level# flex flex-col ">
+            <div className="flex items-center pl-[47px] mb-[6px]">
+              <span className="euro-style black-text text-[30px] mr-4">
+                Level
+              </span>
+              <svg
+                width="45"
+                height="45"
+                viewBox="0 0 45 45"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M30.7201 17.0199V30.7201M30.7201 30.7201H17.0199M30.7201 30.7201L14.2799 14.2799"
+                  stroke="#433BFF"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <div className="flex pl-[47px] mb-[24px]">
+              <div className="lt-level-box flex flex-col w-[90px] h-[70px] mr-[15px] cursor-pointer">
+                <span className="poppins text-[30px]">1</span>
+                <span className="euro-style text-[15px] -mt-1">complete</span>
+              </div>
+              <div className="lt-level-box lt-level-box-current flex flex-col w-[90px] h-[70px] mr-[15px]">
+                <span className="poppins text-[30px]">2</span>
+                <span className="euro-style text-[15px] -mt-1">review</span>
+              </div>
+              <div className="lt-level-box flex flex-col w-[90px] h-[70px] mr-[15px]">
+                <span className="poppins text-[30px]">3</span>
+                <span className="euro-style text-[15px] -mt-1">review</span>
+              </div>
+            </div>
+          </div>
+          <div className="level# flex flex-col ">
+            <div className="flex items-center pl-[47px] mb-[6px]">
+              <span className="euro-style black-text text-[30px] mr-4">
+                Learned
+              </span>
+              <svg
+                width="45"
+                height="45"
+                viewBox="0 0 45 45"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M30.7201 17.0199V30.7201M30.7201 30.7201H17.0199M30.7201 30.7201L14.2799 14.2799"
+                  stroke="#433BFF"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <div className="flex pl-[47px] mb-[24px]">
+              <div className="lt-learned-box flex flex-col justify-center items-center w-[90px] h-[70px] mr-[15px]">
+                <span className="poppins text-[30px]">0</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default LeitnerBox;
